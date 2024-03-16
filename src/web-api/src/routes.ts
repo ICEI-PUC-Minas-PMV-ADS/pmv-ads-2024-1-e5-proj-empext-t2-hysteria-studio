1868c154d26c64394af9bdfbed3de483aca22053
Patrick Douglas <pdouglas.s@hotmail.com>
2024-03-16 08:29:03 -0300

    criacao servico

--- src/web-api/src/routes.ts
+++ src/web-api/src/routes.ts
@@ -1,5 +1,10 @@
 import { Router, Request, Response } from 'express'
+import { CriarServicoController } from './controllers/servico/CriarServicoController'
 
 const router = Router();
+
+
+// Rotas tipos de serviço
+router.post('/servico', new CriarServicoController().handle)
 
 export { router };
