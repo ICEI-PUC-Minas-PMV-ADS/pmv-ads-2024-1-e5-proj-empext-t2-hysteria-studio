f7ce8a88bd63d7b5e171846785f24b0d4ac6418a
Patrick Douglas <pdouglas.s@hotmail.com>
2024-03-17 10:51:52 -0300

    Listar  Servico

--- src/web-api/src/routes.ts
+++ src/web-api/src/routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express'
 import { CriarServicoController } from './controllers/servico/CriarServicoController'
 import { AtualizarServicoController } from './controllers/servico/AtualizarServicoController';
+import { ListarServicoController } from './controllers/servico/ListarServicoController';
 
 const router = Router();
 
@@ -8,5 +9,6 @@ const router = Router();
 // Rotas tipos de serviço
 router.post('/servico', new CriarServicoController().handle)
 router.put('/servico', new AtualizarServicoController().handle)
+router.get('/servicos', new ListarServicoController().handle)
 
 export { router };
