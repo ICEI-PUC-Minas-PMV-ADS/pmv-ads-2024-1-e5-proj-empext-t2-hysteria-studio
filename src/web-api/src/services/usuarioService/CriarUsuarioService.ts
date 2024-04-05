f66a26c219299dbaab955b08d4e3e48d6dbd3080
Stephanie Ingrid <stephanieingrid5@gmail.com>
2024-04-05 14:11:01 -0300

    Usuario Service

---
+++ src/web-api/src/services/usuarioService/CriarUsuarioService.ts
@@ -0,0 +1,53 @@
+import prismaClient from '../../prisma';
+import { isEmail } from 'validator';
+
+interface UsuarioRequest {
+    nome: string;
+    cpf: string;
+    data_de_nascimento: Date;
+    telefone: string;
+    endereco: string;
+    email: string;
+    flag_maior_idade: number;
+    responsavel: string;
+    login: string;
+    senha: string;
+    flag_admin: number;
+}
+
+class CriarUsuarioService {
+    async execute({ nome, cpf, data_de_nascimento, telefone, endereco, email, flag_maior_idade, responsavel, login, senha, flag_admin }: UsuarioRequest) {
+        if (!nome || !cpf || !data_de_nascimento || !telefone || !endereco || !email || !flag_maior_idade || !responsavel || !login || !senha || !flag_admin) {
+            throw new Error("Dados incompletos, verifique os campos");
+        }
+
+        if (!isEmail(email)) {
+            throw new Error('Email inválido');
+        }
+
+        const dataNascimento = new Date(data_de_nascimento);
+
+        const dt_criacao = new Date();
+
+        const usuario = await prismaClient.usuario.create({
+            data: {
+                nome,
+                cpf,
+                data_de_nascimento: dataNascimento,
+                telefone,
+                endereco,
+                email,
+                flag_maior_idade,
+                responsavel,
+                login,
+                senha,
+                flag_admin,
+                dt_criacao,
+            }
+        });
+
+        return usuario;
+    }
+}
+
+export { CriarUsuarioService };
