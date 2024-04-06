495f4bf9acebc60fe1a396516d462d5a033aa1d8
Luiz Fernando <luizfernandorb.fer@gmail.com>
2024-04-06 19:34:50 -0300

    atualizacao-cod

--- src/web-api/src/services/agenda/NovaAgendaService.ts
+++ src/web-api/src/services/agenda/NovaAgendaService.ts
@@ -1,27 +1,27 @@
 import prismaClient from "../../prisma";
 
-interface ScheduleRequest{
+interface AgendaRequest{
   user_id: string;
   servico_id: string;
   cliente: string;
 }
 
 class NovaAgendaService{
-  async execute({ user_id, servico_id, cliente}: ScheduleRequest){
+  async execute({ user_id, servico_id, cliente}: AgendaRequest){
 
     if(cliente === '' || servico_id === ''){
       throw new Error("Error schedule new service.")
     }
 
-  //   const agenda = await prismaClient.servico.create({
-  //     data:{
-  //       cliente,
-  //       servico_id,
-  //       user_id
-  //     }
-  //   })
+    const agenda = await prismaClient.servico.create({
+      data:{
+        cliente,
+        servico_id,
+        user_id
+      }
+    })
 
-  //  return agenda;
+   return agenda;
   }
 }
 
