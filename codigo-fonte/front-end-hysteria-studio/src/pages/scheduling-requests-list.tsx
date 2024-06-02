6854a0f26fdde02786966919431a4354d168c98a
AmandaCacholi <amanda.cacholi@outlook.com>
2024-06-02 11:32:33 -0300

    feat: ajustar listagem

--- codigo-fonte/front-end-hysteria-studio/src/pages/scheduling-requests-list.tsx
+++ codigo-fonte/front-end-hysteria-studio/src/pages/scheduling-requests-list.tsx
@@ -1,57 +1,61 @@
 import { Box, LinearProgress, Paper, Typography } from "@mui/material";
 import TableSchedulingLists from "../components/table-scheduling-lists";
-import { GetPedidosAgendamentosResult, useGetAgendamentosRequestQuery } from "../services/endpoins";
+import {
+  GetAgendamentosResult,
+  useGetAgendamentosQuery,
+} from "../services/endpoins";
 
-const SchedulingRequestList = () => {
-    const {
-        data: agendamentos,
-        isFetching: isFetchingAgendamentos,
-        isError: isErrorAgendamentos,
-    } = useGetAgendamentosRequestQuery(undefined, {
-        selectFromResult: (result) => ({
-            ...result,
-            data: result.data?.filter(
-                (agendamento) => !agendamento.status_agendamento_confirmado
-            ),
-        }),
-    });
-        
-    return (
-        <Box component={Paper} display="flex" flexDirection="column" p={2}>
-            {isFetchingAgendamentos ? (
-                <Box component={Paper} p={2} mt={2}>
-                    <LinearProgress />
-                </Box>
-            ) : isErrorAgendamentos ? (
-                <Box
-                component={Paper}
-                p={2}
-                mt={2}
-                display="flex"
-                justifyContent="center"
-                >
-                <Typography fontWeight="bold">
-                Ocorreu um erro ao buscar as informações.
-                </Typography>
-                </Box>
-            ) : !agendamentos?.length ? (
-                <Box
-                component={Paper}
-                p={2}
-                mt={2}
-                display="flex"
-                justifyContent="center"
-            >
-            <Typography fontWeight="bold">Não há históricos.</Typography>
-            </Box>
-            ) : (
-                <TableSchedulingLists
-                data={agendamentos as Array<GetPedidosAgendamentosResult>}
-                listType="requests"
-                />
-            )}
-            </Box>
-    );
+const SchedulingRequestsList = () => {
+  const {
+    data: agendamentos,
+    isFetching: isFetchingAgendamentos,
+    isError: isErrorAgendamentos,
+  } = useGetAgendamentosQuery(undefined, {
+    selectFromResult: (result) => ({
+      ...result,
+      data: result.data?.filter(
+        (agendamento) =>
+          agendamento.status.status_agendamento === "SOLICITACAO_EM_ESPERA"
+      ),
+    }),
+  });
+
+  return (
+    <Box component={Paper} display="flex" flexDirection="column" p={2}>
+      {isFetchingAgendamentos ? (
+        <Box component={Paper} p={2} mt={2}>
+          <LinearProgress />
+        </Box>
+      ) : isErrorAgendamentos ? (
+        <Box
+          component={Paper}
+          p={2}
+          mt={2}
+          display="flex"
+          justifyContent="center"
+        >
+          <Typography fontWeight="bold">
+            Ocorreu um erro ao buscar as informações.
+          </Typography>
+        </Box>
+      ) : !agendamentos?.length ? (
+        <Box
+          component={Paper}
+          p={2}
+          mt={2}
+          display="flex"
+          justifyContent="center"
+        >
+          <Typography fontWeight="bold">Não há agendamentos.</Typography>
+        </Box>
+      ) : (
+        <TableSchedulingLists
+          data={agendamentos as Array<GetAgendamentosResult>}
+          listType="requests"
+        />
+      )}
+    </Box>
+  );
 };
 
-export default SchedulingRequestList;
+export default SchedulingRequestsList;
